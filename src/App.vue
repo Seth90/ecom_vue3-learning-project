52052f79dd655494f22d4afa01631e9a2135b0a2
agorbachev <agorbachev@speclab.loc>
2023-01-20 17:18:26 +0300

    started

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Header from './components/Layout/Header.vue'
+</script>
+
+<template>
+  <div class="container">
+    <Header/>
+  </div>
+</template>
+
+<style lang="scss">
+@import url('https://fonts.cdnfonts.com/css/satoshi');
+@import url('https://fonts.cdnfonts.com/css/clash-display');
+* {
+    box-sizing: border-box;
+}
+body {
+    font-family: 'Satoshi', sans-serif;
+}
+.container {
+    max-width: 1440px;
+    margin: auto;
+}
+</style>
---
+++ src/components/Layout/Header.vue
@@ -0,0 +1,52 @@
+<template>
+    <header class="header">
+        <div class="header-top">
+            <div class="header-top-search">
+                <img src="/svg/header-search.svg" alt="Search">
+            </div>
+            <a href="#" class="header-logo">Avion</a>
+            <div class="header-top-right">
+                <div href="#" class="header-top-right__cart">
+                    <a href="">
+                        <img src="/svg/header-cart.svg" alt="cart">
+                    </a>
+                </div>
+                <div href="#" class="header-top-righh__user">
+                    <a href="">
+                        <img src="/svg/header-user.svg" alt="cart">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+.header {
+    height: 132px;
+    background: white;
+    &-top {
+        display: flex;
+        justify-content: space-between;
+        height: 70px;
+        align-items: center;
+        &-right {
+            display: flex;
+            align-items: center;
+            
+            &__cart {
+                margin-right: 16px;
+            }
+        }
+    }
+    &-logo {
+        color: #22202E;
+        font-size: 24px;
+        text-decoration: none;
+        font-family: 'Clash Display', sans-serif;
+        &:hover {
+            text-decoration: underline;
+        }
+    }
+}
+</style>
